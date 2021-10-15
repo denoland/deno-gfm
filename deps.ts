2429bafae81b64b66697b304945af7f6d0f8c335
Luca Casonato <hello@lcas.dev>
2021-10-15 21:27:16 +0200

    fix

--- deps.ts
+++ deps.ts
@@ -6,8 +6,8 @@
 
 export { emojify } from "https://deno.land/x/emoji@0.1.2/mod.ts";
 
-export { default as marked } from "https://esm.sh/marked";
+export { default as marked } from "https://esm.sh/marked@3.0.7";
 
 export * as Prism from "https://esm.sh/prismjs@1.25.0";
 
-export { default as sanitizeHtml } from "http://esm.sh/sanitize-html";
+export { default as sanitizeHtml } from "https://esm.sh/sanitize-html@2.5.2";
