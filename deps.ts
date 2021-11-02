e10f8ee641c3152bdd4016b956315a4fb0cf7fd1
Luca Casonato <hello@lcas.dev>
2021-11-02 11:27:11 +0100

    Revert "lock esm.sh versions"
    This reverts commit f1cf877e56ef8c902a9ff657ab35d5753f85c0f9.

--- deps.ts
+++ deps.ts
@@ -3,18 +3,13 @@
 /// <reference lib="dom.iterable" />
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
-/// <reference lib="deno.unstable" />
 
 export { emojify } from "https://deno.land/x/emoji@0.1.2/mod.ts";
 
-// @deno-types="https://cdn.esm.sh/v53/@types/marked@3.0.1/index.d.ts"
-export { default as marked } from "https://cdn.esm.sh/v53/marked@3.0.7/deno/marked.js";
+export { default as marked } from "https://esm.sh/marked@3.0.7";
 
-// @deno-types="https://cdn.esm.sh/v53/@types/prismjs@1.16.6/index.d.ts"
-export * as Prism from "https://cdn.esm.sh/v53/prismjs@1.25.0/deno/prismjs.js";
+export * as Prism from "https://esm.sh/prismjs@1.25.0";
 
-// @deno-types="https://cdn.esm.sh/v53/@types/sanitize-html@2.5.0/index.d.ts"
-export { default as sanitizeHtml } from "https://cdn.esm.sh/v53/sanitize-html@2.5.2/deno/sanitize-html.js";
+export { default as sanitizeHtml } from "https://esm.sh/sanitize-html@2.5.2";
 
-// @deno-types="https://cdn.esm.sh/v53/@types/he@1.1.2/index.d.ts"
-export { escape as htmlEscape } from "https://cdn.esm.sh/v53/he@1.2.0/deno/he.js";
+export { escape as htmlEscape } from "https://esm.sh/he@1.2.0";
