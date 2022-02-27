907813c8ac11e7f59bf766e4d3e5f76a5aef30a4
crowlkats <crowlkats@toaxl.com>
2022-02-27 12:52:30 +0100

    fix indent codeblocks

--- mod.ts
+++ mod.ts
@@ -15,7 +15,7 @@ class Renderer extends marked.Renderer {
 
   code(code: string, language: string) {
     // a language of `ts, ignore` should really be `ts`
-    language = language.split(",")[0];
+    language = language?.split(",")?.[0];
     const grammar = Object.hasOwnProperty.call(Prism.languages, language)
       ? Prism.languages[language]
       : undefined;
