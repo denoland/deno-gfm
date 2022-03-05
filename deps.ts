0bfc539613baca4dde5173fc672247961ea10d88
Luca Casonato <hello@lcas.dev>
2022-03-05 01:52:28 +0100

    update deps

--- deps.ts
+++ deps.ts
@@ -7,10 +7,10 @@
 
 export { emojify } from "https://deno.land/x/emoji@0.1.2/mod.ts";
 
-export { default as marked } from "https://esm.sh/marked@3.0.7";
+export * as Marked from "https://esm.sh/marked@4.0.12";
 
-export { default as Prism } from "https://esm.sh/prismjs@1.25.0";
+export { default as Prism } from "https://esm.sh/prismjs@1.27.0";
 
-export { default as sanitizeHtml } from "https://esm.sh/sanitize-html@2.6.1";
+export { default as sanitizeHtml } from "https://esm.sh/sanitize-html@2.7.0";
 
 export { escape as htmlEscape } from "https://esm.sh/he@1.2.0";
--- mod.ts
+++ mod.ts
@@ -1,13 +1,13 @@
-import { emojify, htmlEscape, marked, Prism, sanitizeHtml } from "./deps.ts";
+import { emojify, htmlEscape, Marked, Prism, sanitizeHtml } from "./deps.ts";
 import { CSS } from "./style.js";
 export { CSS };
 
-class Renderer extends marked.Renderer {
+class Renderer extends Marked.Renderer {
   heading(
     text: string,
     level: 1 | 2 | 3 | 4 | 5 | 6,
     raw: string,
-    slugger: marked.Slugger,
+    slugger: Marked.Slugger,
   ): string {
     const slug = slugger.slug(raw);
     return `<h${level} id="${slug}"><a class="anchor" aria-hidden="true" tabindex="-1" href="#${slug}"><svg class="octicon octicon-link" viewBox="0 0 16 16" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path></svg></a>${text}</h${level}>`;
@@ -43,7 +43,7 @@ export interface RenderOptions {
 export function render(markdown: string, opts: RenderOptions = {}): string {
   markdown = emojify(markdown);
 
-  const html = marked(markdown, {
+  const html = Marked.marked(markdown, {
     baseUrl: opts.baseUrl,
     gfm: true,
     renderer: new Renderer(),
