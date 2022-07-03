d20a2f9f5c05a847bc2877802698d587372e8ec3
LinusJohannsen <linus.johannsen@outlook.com>
2022-07-03 19:12:24 +0200

    Added highlight-source to allowedClasses

--- mod.ts
+++ mod.ts
@@ -86,7 +86,7 @@ export function render(markdown: string, opts: RenderOptions = {}): string {
       iframe: ["src", "width", "height"], // Only used when iframe tags are allowed in the first place.
     },
     allowedClasses: {
-      div: ["highlight"],
+      div: ["highlight", "highlight-source-*"],
       span: [
         "token",
         "keyword",
