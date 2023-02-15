9f56993bf9841c33a8667e0f64e8dfc1fadd37ba
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-02-15 10:26:02 -0800

    chore: move to deno.json

--- example/main.ts
+++ example/main.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.172.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 
 import { CSS, KATEX_CSS, render } from "../mod.ts";
 
