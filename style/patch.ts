acccf6773c19e6c917d6ef6fdb8fece0ceba1417
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-02-17 09:20:49 -0800

    bump node deps and integrate better with dax

--- style/patch.ts
+++ style/patch.ts
@@ -1,24 +1,29 @@
-import { expandGlobSync } from "https://deno.land/std@0.177.0/fs/expand_glob.ts";
-import $ from "https://deno.land/x/dax@0.26.0/mod.ts";
+import $ from "https://deno.land/x/dax@0.27.0/mod.ts";
 import css from "npm:css";
+
+await $`rm -rf style/node_modules/@primer/primitives`;
+await $`cd style && npm install`;
 
 const colorVariables = new Set<string>();
 const variableRegex = /--[\w-]+/g;
 
-for (
-  const { path } of [
-    { path: "main.scss" },
-    ...expandGlobSync("node_modules/@primer/css/markdown/*.scss"),
-  ]
-) {
-  const file = Deno.readTextFileSync(path);
+const cwd = $.path(".");
+const scssFiles = [
+  cwd.join("./style/main.scss"),
+  ...Array.from(
+    cwd.expandGlobSync("node_modules/@primer/css/markdown/*.scss"),
+  ).map((e) => e.path),
+];
+
+for (const pathRef of scssFiles) {
+  const file = pathRef.textSync();
 
   for (const variable of file.match(variableRegex) ?? []) {
     colorVariables.add(variable);
   }
 }
 
-console.log("Extracted color variables", colorVariables);
+$.logStep("Extracted color variables", colorVariables);
 
 const colorRegex = new RegExp(
   [...colorVariables].map((colorVariable) => `\\s+${colorVariable}.+`).join(
@@ -29,8 +34,8 @@ const colorRegex = new RegExp(
 
 for (const mode of ["light", "dark"]) {
   const primitiveFile =
-    `node_modules/@primer/primitives/dist/scss/colors/_${mode}.scss`;
-  console.log("Patching", primitiveFile);
+    `./style/node_modules/@primer/primitives/dist/scss/colors/_${mode}.scss`;
+  $.logStep("Patching", primitiveFile);
   const colorPrimitive = Deno.readTextFileSync(
     primitiveFile,
   );
@@ -45,19 +50,18 @@ for (const mode of ["light", "dark"]) {
   );
 }
 
-await $`npx parcel build main.scss --no-source-maps`;
+await $`cd style && npx parcel build main.scss --no-source-maps`;
 
 // KATEX
 
-console.log("Fetching katex styles");
+$.logStep("Fetching katex styles");
 const KATEX_BASE_URL = "https://cdn.jsdelivr.net/npm/katex@0.16.4/dist";
-const req = await fetch(`${KATEX_BASE_URL}/katex.min.css`);
-let KATEX_CSS = await req.text();
+let KATEX_CSS = await $.request(`${KATEX_BASE_URL}/katex.min.css`).text();
 
 // Replace url of fonts with a cdn since we aren't packaging these
 KATEX_CSS = KATEX_CSS.replaceAll("fonts/", `${KATEX_BASE_URL}/fonts/`);
 
-console.log("Extracting katex classes");
+$.logStep("Extracting katex classes");
 const KATEX_CSS_AST = css.parse(KATEX_CSS);
 const KATEX_CSS_RULES = KATEX_CSS_AST.stylesheet.rules.filter((
   rule: { type: string },
@@ -80,11 +84,11 @@ for (const selector of KATEX_CSS_SELECTORS) {
 // de-duplicate classes
 classes = [...new Set(classes)];
 
-console.log("Writing the final style.js");
-const CSS = await Deno.readTextFile("./dist/main.css");
+$.logStep("Writing the final style.js");
+const CSS = await Deno.readTextFile("./style/dist/main.css");
 
 await Deno.writeTextFile(
-  "style.js",
+  "./style.js",
   `/** @type {string} */
 export const CSS = \`${CSS}\`;
 
