29c53c637cd47e10577e907503e4a5b35a4655c5
hashrock <hashrock@users.noreply.github.com>
2023-03-09 22:34:19 +0900

    test: add math rendering

---
+++ test/fixtures/math.md
@@ -0,0 +1,5 @@
+Block math:
+
+$$ y = x^2 $$
+
+Inline math: $y = x^2$
--- test/test.ts
+++ test/test.ts
@@ -2,15 +2,29 @@ import { assertEquals } from "https://deno.land/std@0.178.0/testing/asserts.ts";
 import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.35-alpha/deno-dom-wasm.ts";
 import { render } from "../mod.ts";
 
-Deno.test("basic markdown", async () => {
+Deno.test("Basic markdown", async () => {
   const markdown = await Deno.readTextFile("./test/fixtures/basic.md");
   const expected = await Deno.readTextFile("./test/fixtures/basic.html");
   const html = render(markdown);
   assertEquals(html, expected);
 
   const document = new DOMParser().parseFromString(
-    html, "text/html"
+    html,
+    "text/html",
   );
   assertEquals(document?.querySelector("h1")?.textContent, "Heading");
   assertEquals(document?.querySelectorAll("li")?.length, 3);
+});
+
+Deno.test("Math rendering", async () => {
+  const math = await Deno.readTextFile("./test/fixtures/math.md");
+  const html = render(math, { allowMath: true });
+  const document = new DOMParser().parseFromString(
+    html,
+    "text/html",
+  );
+  assertEquals(
+    document?.querySelector(".katex-mathml")?.textContent,
+    "y=x2y = x^2",
+  );
 });
