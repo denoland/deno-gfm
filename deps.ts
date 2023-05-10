b31e8ebc9b9b85cecc64cabe3d8de439d588c0f4
Roj <ez@roj.im>
2023-05-10 21:02:19 +0300

    Format

--- deps.ts
+++ deps.ts
@@ -9,7 +9,7 @@ export { emojify } from "https://deno.land/x/emoji@0.2.1/mod.ts";
 
 export * as Marked from "https://esm.sh/marked@5.0.1";
 
-export {gfmHeadingId} from 'https://esm.sh/marked-gfm-heading-id@3.0.3';
+export { gfmHeadingId } from "https://esm.sh/marked-gfm-heading-id@3.0.3";
 
 export { default as Prism } from "https://esm.sh/prismjs@1.29.0";
 
