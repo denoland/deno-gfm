9f4d92c42c92ea1979204032b1dd5956281b7978
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-07-03 09:19:40 -0700

    chore: bump dependencies

--- deps.ts
+++ deps.ts
@@ -5,11 +5,11 @@
 /// <reference lib="deno.ns" />
 /// <reference lib="deno.unstable" />
 
-export { emojify } from "https://deno.land/x/emoji@0.2.1/mod.ts";
+export { emojify } from "https://deno.land/x/emoji@0.3.0/mod.ts";
 
-export * as Marked from "https://esm.sh/marked@5.0.1";
+export * as Marked from "https://esm.sh/marked@5.1.0/";
 
-export { gfmHeadingId } from "https://esm.sh/marked-gfm-heading-id@3.0.3";
+export { gfmHeadingId } from "https://esm.sh/marked-gfm-heading-id@3.0.4/";
 
 export { default as Prism } from "https://esm.sh/prismjs@1.29.0";
 
@@ -17,4 +17,4 @@ export { default as sanitizeHtml } from "https://esm.sh/sanitize-html@2.8.1?targ
 
 export { escape as htmlEscape } from "https://esm.sh/he@1.2.0";
 
-export { default as katex } from "https://esm.sh/katex@0.16.4/dist/katex.mjs";
+export { default as katex } from "https://esm.sh/katex@0.16.8/dist/katex.mjs";
--- example/main.ts
+++ example/main.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.192.0/http/server.ts";
 
 import { CSS, KATEX_CSS, render } from "../mod.ts";
 
