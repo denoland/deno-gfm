b8c429c3365051f98c8a1715841b8ca2e2b2a71f
Reed von Redwitz <reed+github@vonredwitz.com>
2024-01-12 19:32:11 +0100

    chore: bump deps (#86)

--- deps.ts
+++ deps.ts
@@ -7,18 +7,18 @@
 
 export { emojify } from "https://deno.land/x/emoji@0.3.0/mod.ts";
 
-export * as Marked from "https://esm.sh/marked@9.1.1?pin=v135";
+export * as Marked from "https://esm.sh/marked@11.1.1?pin=v135";
 
 export { default as GitHubSlugger } from "https://esm.sh/github-slugger@2.0.0?pin=v135";
 
 export { default as markedAlert } from "https://esm.sh/marked-alert@2.0.1?pin=v135";
 
-export { gfmHeadingId } from "https://esm.sh/marked-gfm-heading-id@3.1.0?pin=v135";
+export { gfmHeadingId } from "https://esm.sh/marked-gfm-heading-id@3.1.2?pin=v135";
 
 export { default as Prism } from "https://esm.sh/prismjs@1.29.0?pin=v135";
 
-export { default as sanitizeHtml } from "https://esm.sh/sanitize-html@2.8.1?target=esnext&pin=v135";
+export { default as sanitizeHtml } from "https://esm.sh/sanitize-html@2.11.0?target=esnext&pin=v135";
 
 export { escape as htmlEscape } from "https://esm.sh/he@1.2.0?pin=v135";
 
-export { default as katex } from "https://esm.sh/katex@0.16.8/dist/katex.mjs?pin=v135";
+export { default as katex } from "https://esm.sh/katex@0.16.9/dist/katex.mjs?pin=v135";
