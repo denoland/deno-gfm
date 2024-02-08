c6de7cfaf75542a80b67ee0559364ec5153dcd83
Luca Casonato <hello@lcas.dev>
2024-02-08 14:40:33 -0800

    fix

--- mod.ts
+++ mod.ts
@@ -9,7 +9,7 @@ import sanitizeHtml from "sanitize-html";
 import he from "he";
 import katex from "katex";
 
-import { CSS, KATEX_CLASSES, KATEX_CSS } from "./style.js";
+import { CSS, KATEX_CLASSES, KATEX_CSS } from "./style.ts";
 export { CSS, KATEX_CSS, Marked };
 
 Marked.marked.use(markedAlert());
@@ -47,11 +47,11 @@ export class Renderer extends Marked.Renderer {
     return `<h${level} id="${slug}"><a class="anchor" aria-hidden="true" tabindex="-1" href="#${slug}"><svg class="octicon octicon-link" viewBox="0 0 16 16" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path></svg></a>${text}</h${level}>\n`;
   }
 
-  image(src: string, title: string | null, alt: string) {
+  image(src: string, title: string | null, alt: string): string {
     return `<img src="${src}" alt="${alt}" title="${title ?? ""}" />`;
   }
 
-  code(code: string, language?: string) {
+  code(code: string, language?: string): string {
     // a language of `ts, ignore` should really be `ts`
     // and it should be lowercase to ensure it has parity with regular github markdown
     language = language?.split(",")?.[0].toLocaleLowerCase();
@@ -72,7 +72,7 @@ export class Renderer extends Marked.Renderer {
     return `<div class="highlight highlight-source-${language} notranslate"><pre>${html}</pre></div>`;
   }
 
-  link(href: string, title: string | null, text: string) {
+  link(href: string, title: string | null, text: string): string {
     const titleAttr = title ? ` title="${title}"` : "";
     if (href.startsWith("#")) {
       return `<a href="${href}"${titleAttr}>${text}</a>`;
