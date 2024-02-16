23f3ee445f935ddfa6164f7f744ae5d9e49d919d
Reed von Redwitz <reed+github@vonredwitz.com>
2024-02-16 10:53:33 +0100

    fix: more tweaks to improve remark compatibility

--- mod.ts
+++ mod.ts
@@ -16,6 +16,16 @@ export { CSS, KATEX_CSS, Marked };
 Marked.marked.use(markedAlert());
 Marked.marked.use(gfmHeadingId());
 Marked.marked.use(markedFootnote());
+Marked.marked.use({
+  walkTokens: (token) => {
+    // putting a list inside a summary requires a double line break
+    // but we shouldn't keep that double line break in the output
+    // this doesn't happen in remark/rehype
+    if (token.type === "html" && token.text.endsWith("</summary>\n\n")) {
+      token.text = token.text.replace("</summary>\n\n", "</summary>\n");
+    }
+  },
+});
 
 export class Renderer extends Marked.Renderer {
   allowMath: boolean;
@@ -35,7 +45,7 @@ export class Renderer extends Marked.Renderer {
     raw: string,
   ): string {
     const slug = this.#slugger.slug(raw);
-    return `<h${level} id="${slug}"><a class="anchor" aria-hidden="true" tabindex="-1" href="#${slug}"><svg class="octicon octicon-link" viewBox="0 0 16 16" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path></svg></a>${text}</h${level}>`;
+    return `<h${level} id="${slug}"><a class="anchor" aria-hidden="true" tabindex="-1" href="#${slug}"><svg class="octicon octicon-link" viewBox="0 0 16 16" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path></svg></a>${text}</h${level}>\n`;
   }
 
   image(src: string, title: string | null, alt: string) {
@@ -287,7 +297,7 @@ export function render(markdown: string, opts: RenderOptions = {}): string {
     h5: ["id"],
     h6: ["id"],
     li: ["id"],
-    td: ["colspan", "rowspan", "align"],
+    td: ["colspan", "rowspan", "align", "width"],
     iframe: ["src", "width", "height"], // Only used when iframe tags are allowed in the first place.
     math: ["xmlns"], // Only enabled when math is enabled
     annotation: ["encoding"], // Only enabled when math is enabled
