a6b98ec628de4e9ae32bdba16e8d4416219de537
mattvr <4052466+mattvr@users.noreply.github.com>
2024-10-17 21:34:59 -0700

    fix: broken package / import assertion (#128)

--- mod.ts
+++ mod.ts
@@ -39,16 +39,20 @@ export class Renderer extends Marked.Renderer {
     this.#slugger = new GitHubSlugger();
   }
 
-  heading(text: string, level: 1 | 2 | 3 | 4 | 5 | 6, raw: string): string {
+  override heading(
+    text: string,
+    level: 1 | 2 | 3 | 4 | 5 | 6,
+    raw: string,
+  ): string {
     const slug = this.#slugger.slug(raw);
     return `<h${level} id="${slug}"><a class="anchor" aria-hidden="true" tabindex="-1" href="#${slug}"><svg class="octicon octicon-link" viewBox="0 0 16 16" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path></svg></a>${text}</h${level}>\n`;
   }
 
-  image(src: string, title: string | null, alt: string): string {
+  override image(src: string, title: string | null, alt: string): string {
     return `<img src="${src}" alt="${alt}" title="${title ?? ""}" />`;
   }
 
-  code(code: string, language?: string): string {
+  override code(code: string, language?: string): string {
     const isTitleIncluded = language?.match(/\stitle="(.+)"/);
     let title = null;
     if (isTitleIncluded) {
@@ -79,7 +83,7 @@ export class Renderer extends Marked.Renderer {
     return `<div class="highlight highlight-source-${language} notranslate">${titleHtml}<pre>${html}</pre></div>`;
   }
 
-  link(href: string, title: string | null, text: string): string {
+  override link(href: string, title: string | null, text: string): string {
     const titleAttr = title ? ` title="${title}"` : "";
     if (href.startsWith("#")) {
       return `<a href="${href}"${titleAttr}>${text}</a>`;
@@ -463,7 +467,7 @@ function stripTokens(
 }
 
 class StripTokenizer extends Marked.Tokenizer {
-  codespan(src: string): Marked.Tokens.Codespan | undefined {
+  override codespan(src: string): Marked.Tokens.Codespan | undefined {
     // copied & modified from Marked to remove escaping
     const cap = this.rules.inline.code.exec(src);
     if (cap) {
